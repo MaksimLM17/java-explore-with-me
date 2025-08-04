a9053a8d9e36ca12d6f4baf3d977e19a97124048
Максим <max-282@yandex.ru>
2025-08-04 19:09:19 +0500

    Category case complete

--- ewm-main-service/src/main/java/ru/practicum/category/service/CategoryPublicServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/category/service/CategoryPublicServiceImpl.java
@@ -1,20 +1,40 @@
 package ru.practicum.category.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.practicum.category.dto.CategoryDto;
+import ru.practicum.category.model.Category;
+import ru.practicum.category.repository.CategoryRepository;
+import ru.practicum.exception.NotFoundException;
+import ru.practicum.mapper.CategoryMapper;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryPublicServiceImpl implements CategoryPublicService {
+
+    private final CategoryMapper categoryMapper;
+    private final CategoryRepository categoryRepository;
 
     @Override
     public List<CategoryDto> findAll(Integer from, Integer size) {
-        return List.of();
+        int pageNumber = (int) Math.floor((double) from / size);
+        Pageable pageable = PageRequest.of(pageNumber, size);
+        Page<Category> page = categoryRepository.findAll(pageable);
+
+        return page.getContent().stream()
+                .map(categoryMapper::mapToDto)
+                .toList();
     }
 
     @Override
     public CategoryDto findById(Integer catId) {
-        return null;
+        Category category = categoryRepository.findById(catId)
+                .orElseThrow(() -> new NotFoundException("Категория по id = %d не найдена".formatted(catId)));
+        return categoryMapper.mapToDto(category);
     }
 }
