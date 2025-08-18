67aa673ae23fb852bd36e8b576addf54b4ce62d9
Максим <max-282@yandex.ru>
2025-08-18 07:34:08 +0500

    fix to remarks

--- ewm-main-service/src/main/java/ru/practicum/requests/controller/RequestsController.java
+++ ewm-main-service/src/main/java/ru/practicum/requests/controller/RequestsController.java
@@ -1,7 +1,6 @@
 package ru.practicum.requests.controller;
 
 import jakarta.validation.constraints.Positive;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
